d6cd903d061f63fae701eb72280a35970da2885a
tommasodreossi <tommasodreossi@gmail.com>
2017-04-24 17:05:20 -0700

    heatmap module

---
+++ image_modification/modify_primitives/heatmap.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageDraw
+
+
+# Draw circle on top of picture
+def draw_circle(im, x, y, radius, color):
+    draw = ImageDraw.Draw(im)
+    draw.ellipse((x-(radius/2), y-(radius/2), x+(radius/2), y+(radius/2)), color)
+    del draw
+    return im
+
+
+# Get rgb color from range of values
+def rgb(minimum, maximum, value):
+    minimum, maximum = float(minimum), float(maximum)
+    ratio = 2 * (value-minimum) / (maximum - minimum)
+    b = int(max(0, 255*(1 - ratio)))
+    r = int(max(0, 255*(ratio - 1)))
+    g = 255 - b - r
+    return r, g, b
+
+
+# Save picture
+def save_image(im, path):
+    im.save(path)
+
+
+# Show picture
+def show_image(im):
+    im.show()
+
+
+im = Image.open("/home/tommaso/FalsifyNN/pics/roads/desert.jpg")
+col = rgb(0,100,50)
+im = draw_circle(im,1500,1000,10,col)
+im = draw_circle(im,1400,1000,10,col)
+show_image(im)
+
+
+
+
--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-from populateLibrary import generatePicture
-from ml_primitives import uniform_sampling
-from neural_nets import squeezedet as nn
+# from populateLibrary import generatePicture
+# from ml_primitives import uniform_sampling
+# # from neural_nets import squeezedet as nn
+#
+# PIC_PATH = '/home/tommaso/FalsifyNN/pics/out/test.png'
+#
+# # Initialize neural network
+# # conf = nn.init()
+#
+# # Initialize samples
+# DIM = 2
+# N_SAMPLES = 30
+#
+# samples = uniform_sampling(DIM, N_SAMPLES)
+#
+# # Test loop
+# for sample in samples:
+#     generatePicture(sample,PIC_PATH)
+#     # print( nn.classify(PIC_PATH,conf) )
 
-
-PIC_PATH = '/home/tommaso/FalsifyNN/pics/out/test.png'
-
-# Initialize neural network
-conf = nn.init()
-
-# Initialize samples
-DIM = 2
-N_SAMPLES = 30
-
-samples = uniform_sampling(DIM, N_SAMPLES)
-
-# Test loop
-for sample in samples:
-    generatePicture(sample,PIC_PATH)
-    print( nn.classify(PIC_PATH,conf) )
+from populateLibrary import *
