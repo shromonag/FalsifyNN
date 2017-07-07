a5ca364bce8e4d00a5c37ce77fa5c8d19f945beb
Shromona MacBook <shromona.ghosh@berkeley.edu>
2017-07-06 17:48:16 -0700

    Fixed bo

--- image_modification/modify_primitives/clustering_bo.py
+++ image_modification/modify_primitives/clustering_bo.py
@@ -12,9 +12,9 @@ import GPyOpt
 import numpy as np
 import GPy
 
-def bo_class():
+class bo_class():
     def __init__(self, input_dim):
-        self.bounds = [{'name':'x', 'type':'continuous', 'domain':(0,1), 'dimension':input_dim}]
+        self.bounds = [{'name':'x', 'type':'continuous', 'domain':(0,1), 'dimensionality':input_dim}]
         self.input_dim = input_dim
 
     def init_BO(self, f, kernel=None, constrains=None,  **kwargs):
@@ -25,7 +25,7 @@ def bo_class():
                                                       kernel=self.kernel, **kwargs)
 
     def run_BO(self, max_iter=10, max_time=np.inf):
-        self.bo = self.bo.run_optimization(max_iter=max_iter, max_time=max_time)
+        self.bo.run_optimization(max_iter=max_iter, max_time=max_time)
         self.ce()
 
     def print_output(self, fix_output):
@@ -37,4 +37,5 @@ def bo_class():
     def ce(self):
         self.x_opt = self.bo.x_opt
         self.fx_opt = self.bo.fx_opt
+
 
