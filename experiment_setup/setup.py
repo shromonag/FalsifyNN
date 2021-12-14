69ea23a6bf6799da4ffbc494ad21672c75905a88
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 02:00:33 +0800

    Delete experiment_setup directory

--- experiment_setup/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='experiment_setup',
-      install_requires=[
-          'sympy',
-          'numpy'
-      ],
-      packages=find_packages(),
-)
